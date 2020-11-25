c12316f3c8ca59c1617ebe13df21dbe4f61e0e46
andrey <andreyignatenkot@gmail.com>
2020-11-25 09:49:30 +0800

    modified

--- public/assets/js/paypal.js
+++ public/assets/js/paypal.js
@@ -13,7 +13,7 @@ paypal.Buttons({
         return actions.order.create({
             purchase_units: [{
             amount: {
-                value: '0.01'
+                value: $('#Amount').val()
             }
             }]
         });
@@ -26,39 +26,3 @@ paypal.Buttons({
         });
       }
 }).render('#paypal-button-container');
-
-
-
-//   paypal.Button.render({
-//     env: 'sandbox', // Or 'production'
-//     style: {
-//       size: 'large',
-//       color: 'gold',
-//       shape: 'pill',
-//     },
-//     // Set up the payment:
-//     // 1. Add a payment callback
-//     payment: function (data, actions) {
-//       // 2. Make a request to your server
-//       return actions.request.post('/api/create-paypal-transaction')
-//         .then(function (res) {
-//           // 3. Return res.id from the response
-//           console.log(res);
-//           return res.id
-//         })
-//     },
-//     // Execute the payment:
-//     // 1. Add an onAuthorize callback
-//     onAuthorize: function (data, actions) {
-//       // 2. Make a request to your server
-//       return actions.request.post('/api/confirm-paypal-transaction', {
-//         payment_id: data.paymentID,
-//         payer_id: data.payerID
-//       })
-//         .then(function (res) {
-//           console.log(res)
-//           alert('Payment successfully done!!')
-//           // 3. Show the buyer a confirmation message.
-//         })
-//     }
-//   }, '#paypal-button')
